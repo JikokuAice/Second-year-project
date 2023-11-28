97ed430a3039b3f0c4cbb341c97380bf0c6f81d0
dragonilly <134251470+JikokuAice@users.noreply.github.com>
2023-11-28 19:41:07 +0545

    first commit
    basic file structure for syp (finance 360)

---
+++ fontend/scripts/login.js
@@ -0,0 +1,55 @@
+const email = document.querySelector('#inpute')
+const  pass = document.querySelector('#inputp')
+const evalid = document.querySelector('.emailvalid')
+const pvalid = document.querySelector('.passvalid')
+const submit = document.querySelector('.submit');
+
+
+
+const validemail=(email)=>{
+    var symbol=/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return symbol.test(email);
+}
+
+
+submit.addEventListener('click',(e)=>{
+
+let edata = [];
+let pdata = [];
+
+if(!validemail(email.value)||email.value==null||email.value==""){
+       edata.push('Oops! Email address issue. Please double-check for correctness');  
+}
+
+console.log(evalid)
+   
+if(edata.length > 0){
+    e.preventDefault();
+    evalid.innerText=edata.join(',')
+    email.style.border='1px solid red';
+}else{
+    evalid.innerText="";
+    email.style.border='';
+}
+
+var number = /[0-9]/g;
+var upperCaseLetters = /[A-Z]/g;
+//pass.value<8||pass.value==''||pass.value==null||
+if(!((pass.value.match(number)&&(pass.value.match(upperCaseLetters))))||pass.value<8||pass.value==null||pass.value==""){
+pdata.push("Make it 8+, use capital and number")
+
+}
+
+if(pdata.length>0){
+    e.preventDefault()
+pvalid.innerText=pdata.join(',');
+pass.style.border='1px solid red'
+}else{
+    pvalid.innerText="";
+    pass.style.border=''   
+}
+
+
+})
+
+
